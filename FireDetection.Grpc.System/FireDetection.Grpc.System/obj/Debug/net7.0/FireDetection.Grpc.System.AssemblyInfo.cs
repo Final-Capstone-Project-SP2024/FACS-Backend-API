d479743e9cd77234492c0a10ce82c3ca9b4cedfb
Nắng <99975060+Nang13@users.noreply.github.com>
2024-01-26 12:23:29 +0700

    update

--- FireDetection.Grpc.System/FireDetection.Grpc.System/obj/Debug/net7.0/FireDetection.Grpc.System.AssemblyInfo.cs
+++ FireDetection.Grpc.System/FireDetection.Grpc.System/obj/Debug/net7.0/FireDetection.Grpc.System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FireDetection.Grpc.System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e7f5760dd9e24c06ddc71771b73c103be21042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b11a3abc105f252e6b176f06cc572f386da12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FireDetection.Grpc.System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FireDetection.Grpc.System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
