cabbe486c77618037544adad53c05ebed01ade55
Nắng <99975060+Nang13@users.noreply.github.com>
2024-04-23 09:31:24 +0700

    refactor: genenerate Record

--- FireDetection.Grpc.System/GenerateRecord.py
+++ FireDetection.Grpc.System/GenerateRecord.py
@@ -20,6 +20,8 @@ def generate_percentage():
     return round(random.uniform(0, 100), 2)
 def generate_vote():
     return round(random.uniform(1, 5))
+def generate_AlarmConfig():
+    return round(random.uniform(1,2))
 
 # Specify the desired date range
 start_date = datetime(2023, 10, 1)
@@ -29,23 +31,31 @@ end_date = datetime(2024, 4, 1)
 data = []
 for _ in range(200):
     camera_ids = [
-        "6f88f3aa-209f-4dc0-9f9c-af425b482a88",
-        "19b5d49c-4230-4a6f-8cc8-bbcc16d82554",
-        "036d2831-dd20-4bf0-9637-22de96cb9785",
-        "f5d26584-58cf-46b1-9097-82a6e966dd55"
+        "a1183a6d-0f81-4b46-9fa4-6da368fa3429",
+        "a9d51bba-1d56-4748-ab15-2c44a6e9a0d0",
+        "0c1dff32-7819-4531-8811-86f4ef6765cf"
+    ]
+    
+    recommend_alarm = [
+        'Alarm Level 1',
+         'Alarm Level 2',
+          'Alarm Level 3',
+        'Alarm Level 4',
+         'Alarm Level 5', 
     ]
     
     record = {
         'Id': generate_guid(),
         'RecordTime': generate_datetime(start_date, end_date),
         'Status': random.choice(['InFinish']),
-        'UserRatingPercent': generate_vote(),
         'PredictedPercent': generate_percentage(),
         'CameraID': random.choice(camera_ids),
         'RecordTypeID': 1,
         'CreatedDate': generate_datetime(start_date, end_date),
         'CreatedBy': "00000000-0000-0000-0000-000000000000",
-        'FinishAlarmTime' : generate_datetime(start_date, end_date)
+        'FinishAlarmTime' : generate_datetime(start_date, end_date),
+        'AlarmConfigurationId' : generate_AlarmConfig(),
+        'RecommendAlarmLevel' : random.choice(recommend_alarm)
     }
     data.append(record)
 
